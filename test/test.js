bfa7df1404b0dd0da1bd22184807915d1a030ba8
SevereOverfl0w <overfl0w@narna.co>
2015-01-14 12:59:28 +0000

    Make packages be meta.

--- test/test.js
+++ test/test.js
@@ -72,8 +72,8 @@ describe('Buycraft', function(){
     it('should parse the gold category on IC and find a pouch', function(done){
       icBuycraft.scrape('/category/371012', function(err, scraped){
         if(err) throw err;
-        expect(scraped).to.have.property('packages').that.is.an('array');
-        expect(scraped.packages[0]).to.have.property('name').that.contain('Pouch')
+        expect(scraped.meta).to.have.property('packages').that.is.an('array');
+        expect(scraped.meta.packages[0]).to.have.property('name').that.contain('Pouch')
         done();
       })
     })
